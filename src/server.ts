c183d8bfaa05ee559d09a256dca3586befef0c4e
danubiobwm <danubio.bwm@gmail.com>
2021-12-24 22:52:28 -0300

    Create Client

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from "express";
+import { routes } from './routes';
 
 const app = express();
 
-app.get('/',(request, response)=>{
-  return response.json({
-    message: "Hello world 2"
-  })
-})
+app.use(express.json());
+
+app.use(routes);
+
 
 app.listen(3000, () => console.log("🔥️👨‍💻️Server is running!🐍️💻️"));
